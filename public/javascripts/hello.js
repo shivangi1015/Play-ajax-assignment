ef0fbbead6db81a02bf507954afcb76c32db2402
shivangi <shivangi.gupta@knoldus.in>
2017-03-11 11:15:28 +0530

    fer changes

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,3 +1,41 @@
 if (window.console) {
   console.log("Welcome to your Play application's JavaScript!");
 }
+
+$(document).ready(function(){
+
+
+         $('#btn').on("click",function(){
+         jsRoutes.controllers.signupController.signup().ajax({
+                      success: function(data){
+                        $('#data').html(data);
+                      },
+                      error: function(){
+                      $('#data').html("hello");
+                      alert("fail")
+                    }
+
+          })
+
+
+
+      })
+
+      $('#btn1').on("click",function(){
+                jsRoutes.controllers.LoginController.signin().ajax({
+                            success: function(data){
+                              $('#data').html(data);
+                            },
+                            error: function(){
+                            $('#data').html("hello");
+                            alert("fail")
+                          }
+
+                })
+
+
+            })
+
+
+
+});
